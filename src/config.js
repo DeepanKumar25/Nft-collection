4a4dcd5f5397714374be3c6e4cd02a913fae4293
Deepan Kumar <deepanrvdvenu@gmail.com>
2022-05-19 02:40:50 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "constants/blend_mode.js"));
+const description =
+  "This the the hand-drawn nft collection of aesthetic shirts";
+const baseUri = "ipfs://QmZsopzL8yDuoY7Fr3V2nTFAUnKQ19xcSAx8DfCdtPX7Ye";
+
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 50,
+    layersOrder: [
+      { name: "background" },
+      { name: "shirtbase" },
+      { name: "basedesign" },
+      { name: "handdesign" },
+      { name: "pocket" },
+      { name: "colour" },
+      { name: "tie" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 512,
+  height: 512,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {
+  creator: "RvD",
+};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.width / format.height,
+  imageName: "preview.png",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+};
